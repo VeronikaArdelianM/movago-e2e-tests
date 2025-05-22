08a12241759c79ea97a3d27d501d2454f01466c3
VeronikaArdelianM <ardelian.veronika@chnu.edu.ua>
2025-05-22 16:37:40 +0300

    add test to check gh actions

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -4,3 +4,9 @@ test('homepage loads', async ({ page }) => {
   await page.goto('http://frontend:4200');
   await expect(page.getByText('Вивчайте англійську з MovaGo')).toBeVisible();
 });
+
+test('login', async ({ page }) => {
+  await page.goto('http://frontend:4200');
+  await page.click('text=Увійти');
+  await expect(page.getByText('Немає акаунту?')).toBeVisible();
+});
